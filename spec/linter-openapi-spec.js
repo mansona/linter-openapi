fa5fd5c988809b982520d08e11a34449aca24a7e
Chris Manson <chris@manson.ie>
2020-10-20 17:05:16 +0100

    add a basic test

--- spec/linter-openapi-spec.js
+++ spec/linter-openapi-spec.js
@@ -1,73 +1,24 @@
 'use babel';
 
-import LinterOpenapi from '../lib/linter-openapi';
+import { join } from 'path';
+import { provideLinter } from '../lib/index';
 
-// Use the command `window:run-package-specs` (cmd-alt-ctrl-p) to run specs.
-//
-// To run a specific `it` or `describe` block add an `f` to the front (e.g. `fit`
-// or `fdescribe`). Remove the `f` to unfocus the block.
+const { lint } = provideLinter();
 
 describe('LinterOpenapi', () => {
   let workspaceElement, activationPromise;
 
-  beforeEach(() => {
-    workspaceElement = atom.views.getView(atom.workspace);
-    activationPromise = atom.packages.activatePackage('linter-openapi');
+  beforeEach(async () => {
+    atom.workspace.destroyActivePaneItem();
+    await atom.packages.activatePackage('language-yaml');
+    await atom.packages.activatePackage('linter-openapi');
   });
 
-  describe('when the linter-openapi:toggle event is triggered', () => {
-    it('hides and shows the modal panel', () => {
-      // Before the activation event the view is not on the DOM, and no panel
-      // has been created
-      expect(workspaceElement.querySelector('.linter-openapi')).not.toExist();
+  it('parses valid file with no error', async () => {
+    const OPENAPI = join(__dirname, 'fixtures', 'petstore.yaml');
+    const editor = await atom.workspace.open(OPENAPI);
+    const messages = await lint(editor);
 
-      // This is an activation event, triggering it will cause the package to be
-      // activated.
-      atom.commands.dispatch(workspaceElement, 'linter-openapi:toggle');
-
-      waitsForPromise(() => {
-        return activationPromise;
-      });
-
-      runs(() => {
-        expect(workspaceElement.querySelector('.linter-openapi')).toExist();
-
-        let linterOpenapiElement = workspaceElement.querySelector('.linter-openapi');
-        expect(linterOpenapiElement).toExist();
-
-        let linterOpenapiPanel = atom.workspace.panelForItem(linterOpenapiElement);
-        expect(linterOpenapiPanel.isVisible()).toBe(true);
-        atom.commands.dispatch(workspaceElement, 'linter-openapi:toggle');
-        expect(linterOpenapiPanel.isVisible()).toBe(false);
-      });
-    });
-
-    it('hides and shows the view', () => {
-      // This test shows you an integration test testing at the view level.
-
-      // Attaching the workspaceElement to the DOM is required to allow the
-      // `toBeVisible()` matchers to work. Anything testing visibility or focus
-      // requires that the workspaceElement is on the DOM. Tests that attach the
-      // workspaceElement to the DOM are generally slower than those off DOM.
-      jasmine.attachToDOM(workspaceElement);
-
-      expect(workspaceElement.querySelector('.linter-openapi')).not.toExist();
-
-      // This is an activation event, triggering it causes the package to be
-      // activated.
-      atom.commands.dispatch(workspaceElement, 'linter-openapi:toggle');
-
-      waitsForPromise(() => {
-        return activationPromise;
-      });
-
-      runs(() => {
-        // Now we can test for view visibility
-        let linterOpenapiElement = workspaceElement.querySelector('.linter-openapi');
-        expect(linterOpenapiElement).toBeVisible();
-        atom.commands.dispatch(workspaceElement, 'linter-openapi:toggle');
-        expect(linterOpenapiElement).not.toBeVisible();
-      });
-    });
+    expect(messages.length).toEqual(0);
   });
 });
